fd86a7ed6e1919cf7e5055ab34435253604438a4
ClaudioFabian-A <gonzalez.claudiofabian3@gmail.com>
2023-03-15 23:53:52 -0300

    se culmina un aetapa del proyecto

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@
 
 
 class Producto {
-    constructor(id, nombre, descripcion, precio, ) {
+    constructor(id, nombre, descripcion, precio ) {
         this.id = +id;
         this.nombre = nombre.toUpperCase();
         this.descripcion = descripcion;
         this.precio = parseFloat(precio);
-        this.vendido = false;
+       
 
     }
 
@@ -22,18 +22,33 @@ vender() {
     this.vendido = true;
 }
 }
-const producto1 = new Producto("1","botella", "", 2000);
+const producto1 = new Producto("1", "botella", "", 2000);
 const producto2 = new Producto("2", "taza", "", 800);
 const producto3 = new Producto("3", "cuchara", "", 500);
 const producto4 = new Producto("4", "servilleta", "", 600);
 const producto5 = new Producto("5", "cuchillo", "", 1500);
 
 
-producto1.sumaIva();
-producto2.sumaIva();
-producto1.vender();
 
-let carrito [];
+const datoInicial = prompt( 'coloque 1. 2. 3. 4. 5.');
 
-console.log(producto1.precio, producto1);
+const datoProducto = {
 
+    '1' : producto1,
+    '2' : producto2,
+    '3' : producto3,
+    '4' : producto4,
+    '5' : producto5,
+
+    
+}
+
+const nada = datoProducto[datoInicial] || datoInicial;
+
+
+
+//console.log(nada.nombre);
+console.log(nada.nombre);
+
+
+//console.log(nada.nombre);
