1cb5d6338bfca7bdf44e04405db1ea1c4fa386fa
ClaudioFabian-A <gonzalez.claudiofabian3@gmail.com>
2023-03-18 21:07:34 -0300

    se termina de dar formato al prompt

--- js/index.js
+++ js/index.js
@@ -1,8 +1,3 @@
-
-
-
-//, vendido
-
 class Producto {
     constructor(id, nombre, descripcion, precio) {
         this.id = +id;
@@ -15,75 +10,91 @@ class Producto {
 
 
     }
-
-
     sumaIva() {
         this.precio = this.precio * 1.21;
 
     }
-    // vender() {
-    //     this.vendido = true;
-    // }
 }
-const producto1 = new Producto("1", "botella", "", 2000);
-const producto2 = new Producto("2", "taza", "", 800);
-const producto3 = new Producto("3", "cuchara", "", 500);
-const producto4 = new Producto("4", "servilleta", "", 600);
-const producto5 = new Producto("5", "cuchillo", "", 1500);
-
-const datoProducto = {
-
-    '1': producto1,
-    '2': producto2,
-    '3': producto3,
-    '4': producto4,
-    '5': producto5,
-
-
-}
-const idProducto = ['1', 2, 3, 4, 5]
-
-
-//console.log(producto1);
-let seleccioneProducto = prompt(
-    'Bienvenido a Marpolo.coloque 1.botella 2.taza 3.cuchara 4.servilleta 5.cuchillo'
-)
-console.log(seleccioneProducto.precio);
-
-let seleccionarMasProducto = true;
-const carrito = []
-
-
-
-const pago = prompt('seleccione el medio de pago')
-
-class MedioDePago {
-    constructor(id, metodo, cuotas, monto, tasa) {
-
-        this.id = id;
-        this.metodo = metodo;
-        this.cuotas = cuotas;
-        this.monto = monto;
-        this.tasa = tasa
+const productos = [
+    new Producto(1, "botella", "", 2000),
+    new Producto(2, "taza", "", 800),
+    new Producto(3, "cuchara", "", 500),
+    new Producto(4, "servilleta", "", 600),
+    new Producto(5, "cuchillo", "", 1500),
+]
+let seleccioneProducto = parseInt(prompt(
+    `Bienvenido a Marpolo coloque el numero del producto que desea comprar
+    1.botella, con un valor de $2000.
+    2.taza con un valor de $800
+    3.cuchara con un valor de $500
+    4.servilleta con un valor de $600
+    5.cuchillo con un valor de $1500`
+))
+let eligioProducto = false
+let productoElegido
+while (eligioProducto === false) {
+    productoElegido = productos.find((productoElegido) => productoElegido.id === seleccioneProducto)
+    if (!productoElegido) {
+        seleccioneProducto = parseInt(
+            prompt(
+`Bienvenido a Marpolo coloque CORRECTAMENTE el numero del producto que desea comprar
+    1.botella, con un valor de $2000.
+    2.taza con un valor de $800
+    3.cuchara con un valor de $500
+    4.servilleta con un valor de $600
+    5.cuchillo con un valor de $1500`
+            )
+        )
+    } else {
+        eligioProducto = true;
     }
-
 }
-const efectivo = new MedioDePago("00", 'efectivo', 0, '03', 1.0);
-const cuota001 = new MedioDePago("01", 'efectivo', 0, '03', 1.1);
-const cuota002 = new MedioDePago("02", 'efectivo', 0, '03', 1.2);
-const cuota003 = new MedioDePago("03", 'efectivo', 0, '03', 1.3);
-const cuota006 = new MedioDePago("04", 'efectivo', 0, '03', 1.6);
-const cuota012 = new MedioDePago("05", 'efectivo', 0, '03', 2.0);
+class MedioDePago {
+    constructor(id, metodo, cuotas, monto, tasa,) {
 
-
-const datoMedioDePago = {
-
-    '00': efectivo,
-    '01': cuota001,
-    '02': cuota002,
-    '03': cuota003,
-    '04': cuota006,
-    '05': cuota012,
-
+        this.id = id,
+            this.metodo = metodo,
+            this.cuotas = cuotas,
+            this.monto = monto,
+            this.tasa = tasa
+    }
 }
-//console.log(efectivo);
+const medioDePagoElegido = [
+    new MedioDePago(00, 'efectivo', 0,`${productoElegido.precio}`, 1.0),
+    new MedioDePago(01, 'tarjeta', 1, `${productoElegido.precio}`, 1.1),
+    new MedioDePago(02, 'tarjeta', 2, `${productoElegido.precio}`, 1.2),
+    new MedioDePago(03, 'tarjeta', 3, `${productoElegido.precio}`, 1.3),
+    new MedioDePago(04, 'tarjeta', 6, `${productoElegido.precio}`, 1.6),
+    new MedioDePago(05, 'tarjeta', 12, `${productoElegido.precio}`, 2.0)
+]
+let pago = parseInt(
+    prompt(`El monto del producto seleccionado es de ${productoElegido.precio},
+    lo puede abonar de la sigiente manera:
+    (seleccione el numero correspondiente a el medio de pago que corresponda)
+    00 - Efectivo
+    01 - Tarjeta en 1 pago con un 10% de interes.
+    02 - Tarjeta en 2 pagos con un 20% de interes.
+    03 - Tarjeta en 3 pagos con un 30% de interes.
+    04 - Tarjeta en 6 pagos con un 60% de interes.
+    05 - Tarjeta en 12 pagos con un 20% de interes.`)
+)
+let eligioMedioDePago = false
+let pagos
+while (eligioMedioDePago === false) {
+    pagos = medioDePagoElegido.find((pagos) => pagos.id === pago)
+    if (!pagos) {
+
+        pago = parseInt(
+            prompt('seleccione el medio de pago 01 02 03 04 05')
+        )
+    } else {
+        eligioMedioDePago = true
+    }
+}
+const mult = +`${productoElegido.precio}` * +`${pagos.tasa}`;
+console.log(productoElegido.precio, pagos.tasa, mult);
+alert(`El producto seleccionado es ${productoElegido.nombre},
+      con un valor de ${productoElegido.precio},
+      el cual lo abona en ${pagos.cuotas} cuotas,
+      dando un total consolidado de ${mult}`);
+      alert(`muchas gracias por su compra`)
