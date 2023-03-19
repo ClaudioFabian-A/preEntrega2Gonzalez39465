b01f237be186ff1ae9eacd709739474820e4631a
ClaudioFabian-A <gonzalez.claudiofabian3@gmail.com>
2023-03-18 21:12:27 -0300

    se corrije errores

--- js/index.js
+++ js/index.js
@@ -6,14 +6,8 @@ class Producto {
         this.precio = parseFloat(precio);
         // this.vendido = false;
         this.log = function () { console.log(this.id) }
-
-
-
     }
-    sumaIva() {
-        this.precio = this.precio * 1.21;
 
-    }
 }
 const productos = [
     new Producto(1, "botella", "", 2000),
@@ -37,12 +31,12 @@ while (eligioProducto === false) {
     if (!productoElegido) {
         seleccioneProducto = parseInt(
             prompt(
-`Bienvenido a Marpolo coloque CORRECTAMENTE el numero del producto que desea comprar
+                `Bienvenido a Marpolo coloque CORRECTAMENTE el numero del producto que desea comprar
     1.botella, con un valor de $2000.
-    2.taza con un valor de $800
-    3.cuchara con un valor de $500
-    4.servilleta con un valor de $600
-    5.cuchillo con un valor de $1500`
+    2.taza con un valor de $800.
+    3.cuchara con un valor de $500.
+    4.servilleta con un valor de $600.
+    5.cuchillo con un valor de $1500.`
             )
         )
     } else {
@@ -60,7 +54,7 @@ class MedioDePago {
     }
 }
 const medioDePagoElegido = [
-    new MedioDePago(00, 'efectivo', 0,`${productoElegido.precio}`, 1.0),
+    new MedioDePago(00, 'efectivo', 0, `${productoElegido.precio}`, 1.0),
     new MedioDePago(01, 'tarjeta', 1, `${productoElegido.precio}`, 1.1),
     new MedioDePago(02, 'tarjeta', 2, `${productoElegido.precio}`, 1.2),
     new MedioDePago(03, 'tarjeta', 3, `${productoElegido.precio}`, 1.3),
@@ -92,9 +86,9 @@ while (eligioMedioDePago === false) {
     }
 }
 const mult = +`${productoElegido.precio}` * +`${pagos.tasa}`;
-console.log(productoElegido.precio, pagos.tasa, mult);
+
 alert(`El producto seleccionado es ${productoElegido.nombre},
-      con un valor de ${productoElegido.precio},
+      con un valor de $${productoElegido.precio},
       el cual lo abona en ${pagos.cuotas} cuotas,
-      dando un total consolidado de ${mult}`);
-      alert(`muchas gracias por su compra`)
+      dando un total consolidado de $${mult}.`);
+alert(`Muchas gracias por su compra.`)
